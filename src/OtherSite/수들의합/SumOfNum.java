546ad132a2638e7fb2e625aca829b11b6f82dca5
elane <chagaram0610@gmail.com>
2023-09-20 11:22:44 +0900

    수들의 합, 점수계산 문제 풀이 완료

--- src/OtherSite/수들의합/SumOfNum.java
+++ src/OtherSite/수들의합/SumOfNum.java
@@ -9,9 +9,60 @@ import java.io.InputStreamReader;
 public class SumOfNum {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // 1. N 과 M 입력 받기
         String[] num = bf.readLine().split(" ");
         int N = Integer.parseInt(num[0]);
         int M = Integer.parseInt(num[1]);
+
+        // 2. 수열 입력 받기
+        String [] _seq = bf.readLine().split(" ");
+        // 2-1. 문자열로 받은 수열 int 형으로 형변환
+        int[] seq = new int[N];
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(_seq[i]);
+        }
+        //// 2-2. 배열에 잘 들어갔는지 확인
+        //for(int n : seq){
+        //    System.out.println(n);
+        //}
+
+        // 3. M이 되는 경우의 수 구하기
+
+        int cnt = 0; // 경우의 수
+        for (int i = 0; i < N; i++) {
+            int sum = 0; // M과 같은 수 만들 놈
+            for (int j = i; j < N; j++) {
+                sum += seq[j];
+                if(sum == M) { // M과 같으면 for문 종료
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        // 4. 경우의 수 출력하기
+        System.out.println(cnt);
+
+
+
+
+        //int cnt = 0; // 경우의 수
+        //for (int i = 0; i < N; i++) {
+        //    int sum = seq[i]; // M과 같은 수 만들 놈
+        //    if(sum == M) { // M과 같으면 for문 처음으로 돌아가기
+        //        cnt++;
+        //        continue;
+        //    }
+        //    for (int j = i+1; j < N; j++) {
+        //        sum += seq[j];
+        //        if(sum == M) { // M과 같으면 for문 종료
+        //            cnt++;
+        //            break;
+        //        }
+        //    }
+        //}
+
+
     }
 }
 
---
+++ src/OtherSite/점수계산/ScoreCalculation.java
@@ -0,0 +1,74 @@
+package OtherSite.점수계산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ScoreCalculation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // 1. 문제 개수 N 입력 받기
+        int N = Integer.parseInt(bf.readLine());
+        // 2. 채점 결과 입력 받기
+        String[] _score = bf.readLine().split(" ");
+        // 2-1. 채점 결과 int형으로 형변환
+        int[] mark = new int[N];
+        for (int i = 0; i < N; i++) {
+            mark[i] = Integer.parseInt(_score[i]);
+        }
+        //// 2-2. 배열에 잘 들어갔는지 확인
+        //for (int i = 0; i < N; i++) {
+        //    System.out.println(mark[i]);
+        //}
+
+        // 3. 점수 계산하기
+        int score = 0, cnt = 1; // 점수 담을 놈, 가산점 계산할 놈
+        for (int i = 0; i < N; i++) {
+            // 1인지 비교
+            if(mark[i] == 1){
+                score += cnt++;
+            }else{ // 0 이면 가산점 초기화
+                cnt = 1;
+            }
+        }
+
+        // 4. 점수 출력하기
+        System.out.println(score);
+    }
+
+}
+/*
+        점수계산
+
+        OX 문제는 맞거나 틀린 두 경우의 답을 가지는 문제를 말한다. 여러 개의 OX 문제로 만들어진
+        시험에서 연속적으로 답을 맞히는 경우에는 가산점을 주기 위해서 다음과 같이 점수 계산을 하기
+        로 하였다. 1번 문제가 맞는 경우에는 1점으로 계산한다. 앞의 문제에 대해서는 답을 틀리다가
+        답이 맞는 처음 문제는 1점으로 계산한다. 또한, 연속으로 문제의 답이 맞는 경우에서 두 번째
+        문제는 2점, 세 번째 문제는 3점, ..., K번째 문제는 K점으로 계산한다. 틀린 문제는 0점으로 계
+        산한다.
+
+        예를 들어, 아래와 같이 10 개의 OX 문제에서 답이 맞은 문제의 경우에는 1로 표시하고, 틀린 경
+        우에는 0으로 표시하였을 때, 점수 계산은 아래 표와 같이 계산되어, 총 점수는
+        1+1+2+3+1+2=10 점이다.
+
+        1 0 1 1 1 0 0 1 1 0
+
+        채점 1 0 1 1 1 0 0 1 1 0
+        점수 1 0 1 2 3 0 0 1 2 0
+        시험문제의 채점 결과가 주어졌을 때, 총 점수를 계산하는 프로그램을 작성하시오.
+
+        ▣ 입력설명
+        첫째 줄에 문제의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 N개 문제의 채점 결과를 나
+        타내는 0 혹은 1이 빈 칸을 사이에 두고 주어진다. 0은 문제의 답이 틀린 경우이고, 1은 문제의
+        답이 맞는 경우이다.
+
+        ▣ 출력설명
+        첫째 줄에 입력에서 주어진 채점 결과에 대하여 가산점을 고려한 총 점수를 출력한다.
+
+        ▣ 입력예제 1
+        10
+        1 0 1 1 1 0 0 1 1 0
+
+        ▣ 출력예제 1
+        10
+*/
