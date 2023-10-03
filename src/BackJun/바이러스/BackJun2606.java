b766f1b444aa75df7b1e65b014a205cc5092a767
elane <chagaram0610@gmail.com>
2023-10-03 11:31:30 +0900

    바이러스 문제 풀이 완료

--- src/BackJun/바이러스/BackJun2606.java
+++ src/BackJun/바이러스/BackJun2606.java
@@ -1,5 +1,41 @@
 package BackJun.바이러스;
 
-public class BackJun2606 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class BackJun2606 {
+    static Scanner scn = new Scanner(System.in);
+    static List<List<Integer>> node = new ArrayList<>();
+    static boolean[] visited;
+    static int cnt = 0;
+    public static void main(String[] args) {
+        int N = scn.nextInt();
+        int M = scn.nextInt();
+        visited = new boolean[N+1];
+        for (int i = 0; i <= N; i++) {
+            node.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            int n1 = scn.nextInt();
+            int n2 = scn.nextInt();
+
+            node.get(n1).add(n2);
+            node.get(n2).add(n1);
+        }
+
+        virus(1);
+        System.out.println(cnt - 1);
+    }
+
+    static void virus(int n){
+        if(!visited[n]){
+            visited[n] = true;
+            cnt++;
+            for (int i = 0; i < node.get(n).size(); i++) {
+                virus(node.get(n).get(i));
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // n 입력 받기
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int[] list = new int[n];
-        for (int i = 0; i < n; i++) {
-            // n 줄 만큼 정수 입력 받기
-            list[i] = Integer.parseInt(br.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. N과 M을 입력 받기
+        String[] num = bf.readLine().split(" ");
+        int N = Integer.parseInt(num[0]);
+        int M = Integer.parseInt(num[1]);
+
+        // 2. 수열 입력 받기
+        String[] _seq = bf.readLine().split(" ");
+        int[] seq = new int[N];
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(_seq[i]);
         }
 
-        Arrays.sort(list);
+        //for (int i = 0; i < N; i++) {
+        //    System.out.println(seq[i]);
+        //}
 
-        for (int el: list) {
-            sb.append(el + "\n");
+        // M이 되는 경우의 수 구하기
+        int cnt = 0; // 경우의 수
+        for (int i = 0; i < N; i++) {
+            int sum = 0; // M이랑 같은지 확인할 놈
+            for (int j = i; j < N; j++) {
+                sum += seq[j];
+                if(sum == M){
+                    cnt++;
+                    break;
+                }
+            }
         }
-        System.out.println(sb);
+
+        // 4. 경우의 수 출력하기
+        System.out.println(cnt);
+
     }
-
 }
+
+/*
+    수들의 합
+    N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다. 이 수열의 i번째 수부터 j번째 수까지의
+        합 A[i]+A[i+1]+…+A[j-1]+A[j]가 M이 되는 경우의 수를 구하는 프로그램을 작성하시오.
+
+        ▣ 입력설명
+        첫째 줄에 N(1≤N≤10,000), M(1≤M≤300,000,000)이 주어진다. 다음 줄에는 A[1], A[2], …,
+        A[N]이 공백으로 분리되어 주어진다. 각각의 A[x]는 30,000을 넘지 않는 자연수이다.
+        ▣ 출력설명
+        첫째 줄에 경우의 수를 출력한다.
+        ▣ 입력예제 1
+        8 3
+        1 2 1 3 1 1 1 2
+        ▣ 출력예제 1
+        5
+*/
